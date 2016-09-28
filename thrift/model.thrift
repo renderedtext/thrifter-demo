f69fb3252d4639babb582470fae580bcc7223050
Predrag Rakic <prakic@renderedtext.com>
2016-09-28 22:04:37 +0200

    Add thrift dir

---
+++ thrift/model.thrift
@@ -0,0 +1,21 @@
+
+
+struct Complex {
+  1: i16 r
+  2: i16 i
+}
+
+struct Status {
+  1: bool success
+  2: string desc
+}
+
+struct Result {
+  1: Status status
+  2: Complex res
+}
+
+service Test {
+  Result add(1: Complex a, 2: Complex b)
+  Result sub(1: Complex a, 2: Complex b)
+}

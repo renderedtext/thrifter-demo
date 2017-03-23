cb9d6492a45924268c3eb8085204393a13092fbc
Predrag Rakic <prakic@renderedtext.com>
2017-03-23 12:47:37 +0100

    service health-check test

--- test/thrift_http_test.exs
+++ test/thrift_http_test.exs
@@ -2,13 +2,21 @@ defmodule Test.ThriftHttpTransport do
   use ExUnit.Case
   alias ThrifterDemoGeneratedClient.HttpClient
 
-  test "serialization" do
+  setup_all do
     Application.put_env(:thrifter_demo_generated_client, :host, "localhost")
     Application.put_env(:thrifter_demo_generated_client, :port, 2999)
+  end
+
+  test "function call" do
     number        = HttpClient.Model.Complex.new( r: 3, i: 0)
     {:ok, result} = HttpClient.add(number, number)
 
     assert result.result.r == 6
     assert result.result.i == 0
+  end
+
+  test "" do
+    {:ok, result} = HttpClient.service_health_check
+    assert result.status_code == 200
   end
 end
